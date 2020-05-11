581a5a12bc55241624d25d1c0a3ac7aae341cfb3
Yoda92 <andersfisker1007@outlook.com>
2020-05-11 12:58:43 +0200

    Navbar moved to own component

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,40 +1,21 @@
 <template>
     <div id="app" class="container-fluid w-100 h-100">
         <router-view class="container" />
-        <div class="navbar navbar-expand-lg navbar-dark bg-custom fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">ModelPage</a>
-                <ul class="nav navbar-nav mr-2">
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/">Home</router-link>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item mr-2">
-                        <router-link class="nav-link" to="/Register">Register</router-link>
-                    </li>
-                    <li class="nav-item" v-if="!isLoggedIn">
-                        <router-link class="nav-link" to="/Login">Login</router-link>
-                    </li>
-                    <li class="nav-item" v-if="isLoggedIn">
-                        <a
-                            type="button"
-                            class="nav-link"
-                            v-on:click="logout"
-                            v-if="!isLoggedIn"
-                        >Log Out</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <globalNavbar />
     </div>
 </template>
+
 <script>
+import globalNavbar from "./components/Navbar.vue";
+
 export default {
     data() {
         return {
             isLoggedIn: false,
         };
+    },
+    components: {
+        globalNavbar
     },
     methods: {
         checkLoggedIn() {
@@ -56,10 +37,8 @@ export default {
     },
 };
 </script>
+
 <style>
-.bg-custom {
-    background-color: #34495e !important;
-}
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
---
+++ web-app/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="navbar navbar-expand-lg navbar-dark bg-custom fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">ModelPage</a>
+            <ul class="nav navbar-nav mr-2">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item mr-2">
+                    <router-link class="nav-link" to="/Register">Register</router-link>
+                </li>
+                <li class="nav-item" v-if="!isLoggedIn">
+                    <router-link class="nav-link" to="/Login">Login</router-link>
+                </li>
+                <li class="nav-item" v-if="isLoggedIn">
+                    <a type="button" class="nav-link" v-on:click="logout" v-if="!isLoggedIn">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style>
+.bg-custom {
+    background-color: #34495e !important;
+}
+</style>
