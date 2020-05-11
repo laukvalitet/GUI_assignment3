43e093cc401a00c49ab3d9bbc7115299aa6e8e02
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-05-11 11:44:52 +0200

    Setup login and cleaned up project

---
+++ web-app/src/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="col-lg-4 col-xl-2 col-12">
+        <h1>Login</h1>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="email">
+                    Email:
+                </label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="email"
+                    placeholder="Email address"
+                    v-model="user.email"
+                />
+            </div>
+            <div class="form-group">
+                <label for="password">
+                    Password:
+                </label>
+                <input
+                    type="password"
+                    class="form-control"
+                    name="password"
+                    placeholder="Password"
+                    v-model="user.password"
+                />
+                <small class="text-danger" v-if="failed">Login failed</small>
+            </div>
+            <button class="btn btn-primary" type="submit">Log in</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { post } from "@/shared/fetch.js";
+export default {
+    data() {
+        return {
+            user: {
+                email: "",
+                password: "",
+            },
+            failed: false,
+        };
+    },
+
+    methods: {
+        handleSubmit() {
+            post("api/Account/Login", this.user).then(async response => {
+                if (response.status === 200) {
+                    this.failed = false;
+                    const responseJson = await response.json();
+                    localStorage.setItem("jwt", responseJson.jwt);
+                } else {
+                    this.handleFailedLogin();
+                }
+            });
+        },
+        handleFailedLogin() {
+            this.failed = true;
+        },
+    },
+};
+</script>
---
+++ web-app/src/shared/fetch.js
@@ -0,0 +1,46 @@
+const url = "https://localhost:5001/";
+
+const makeHeader = () => {
+    const header = {
+        "Content-Type": "application/json",
+    };
+    const jwt = localStorage.getItem("jwt");
+    if (jwt !== null) {
+        header.Authorization = "Bearer" + jwt;
+    }
+    return header;
+};
+
+export const post = (endpoint, data) => {
+    return fetch(url + endpoint, {
+        method: "POST",
+        credentials: "include",
+        withCredentials: true,
+        headers: makeHeader(),
+        body: JSON.stringify(data),
+    });
+};
+
+export const get = (endpoint) =>
+    fetch(url + endpoint, {
+        method: "GET",
+        credentials: "include",
+        withCredentials: true,
+        headers: makeHeader(),
+    });
+
+export const del = (endpoint) =>
+    fetch(url + endpoint, {
+        method: "DELETE",
+        credentials: "include",
+        withCredentials: true,
+        headers: makeHeader(),
+    });
+
+export const put = (endpoint) =>
+    fetch(url + endpoint, {
+        method: "PUT",
+        credentials: "include",
+        withCredentials: true,
+        headers: makeHeader(),
+    });
