39c96aeda1e2da0b75b1f8e9c95d5d758e7538e5
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-05-11 13:50:16 +0200

    removed foo and bar

--- web-app/src/components/Home.vue
+++ web-app/src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-100 h-100">
-        <div class="w-100 h-100" v-if="foo">
+        <div class="w-100 h-100">
             <p>Model: This isnt done yet</p>
         </div>
-        <div class="w-100 h-100" v-if="bar">
+        <div class="w-100 h-100">
             <p>Manager: This isnt done yet</p>
         </div>
     </div>
